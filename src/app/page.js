dd23056d06754b5f8a9403f2899ba98727c943e6
Mofu0224 <111069395+Mofu0224@users.noreply.github.com>
2023-11-03 14:03:19 +0900

    いいいいいい

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <Header>moeeeeeeeeeeeee</Header>
-      <Navibar>FUKAAAAA</Navibar>
+      <Navibar></Navibar>
       <Main01></Main01>
       <Footer></Footer>
     </div>
